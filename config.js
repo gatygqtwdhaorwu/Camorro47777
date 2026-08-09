e92a28fd7711d5b9677fc12f6ff225bef77c1fa2
gatygqtwdhaorwu <fatimagsysnjs@gmail.com>
2026-08-09 21:51:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,4 @@ const API_URL = 'https://camorro47777.onrender.com';
 
 // رابط موقعك على GitHub Pages (اختياري - للعرض فقط)
 // مثال: https://YOUR_USERNAME.github.io/camorro-frontend/
-const SITE_URL = 'https://YOUR_USERNAME.github.io/camorro-frontend/';
+const SITE_URL = 'https://gatygqtwdhaorwu.github.io/Camorro47777/';
